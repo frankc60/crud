512a442e5fbdcbe938c803f81719b90d1ae2a003
frankc60 <frankc60@gmail.com>
2018-02-15 17:59:08 +0000

    mongodb added

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 
@@ -10,16 +13,40 @@ console.log("hello world");
 
 
 
-app.listen(3000, function() {
-    console.log('listening on 3000');
+
+ 
+
+  // Retrieve
+
+
+
+  MongoClient.connect('mongodb://localhost:27017/exampleDb', (err, client) => {
+    if (err) return console.log(err)
+    let db;
+    db = client.db('star-wars-quotes') // whatever your database name is
+    app.listen(3000, () => {
+      console.log('listening on 3000, mongodb connected.')
+    })
+
+    app.get('/', (req, res) => {
+      //res.send('Hello World')
+      db.collection('quotes').find().toArray(function(err, results) {
+        console.log(results)
+        // send HTML file populated with quotes here
+      })
+      res.sendFile(__dirname + '/public/html/index.html');
+    });
+
+    app.post('/quotes', (req, res) => {
+      console.log(req.body);
+    
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+  
+      console.log('saved to database')
+      res.redirect('/')
+    })
   });
 
-  app.get('/', (req, res) => {
-    //res.send('Hello World')
-    res.sendFile(__dirname + '/public/html/index.html')
-  })
 
-  app.post('/quotes', (req, res) => {
-    console.log(req.body)
   })
-
